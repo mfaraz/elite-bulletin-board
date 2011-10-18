f405fbf5f3335b8750fd0411ca020efa8ee663c8
maggot05 <maggot05@f75ce355-b11e-115c-d3de-284b74d13a84>
2011-10-18 20:09:04 +0000

    clearing all RC 1 files to make way for RC 2 files. RC 2 will be using CodeIgniter, resulting in MASSIVE file structure changes.

--- trunk/includes/js/pm.ajax.js
+++
@@ -1,8 +0,0 @@
-/**
- * pm.ajax.js
- * @package Elite Bulletin Board v3
- * @author Elite Bulletin Board Team <http://elite-board.us>
- * @copyright  (c) 2006-2011
- * @license http://opensource.org/licenses/gpl-license.php GNU Public License
- * @version 4/13/2011
-*/
