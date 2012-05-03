ac073e50b4477953490be9ddf8b2ecea93888ac1
maggot05 <maggot05@f75ce355-b11e-115c-d3de-284b74d13a84>
2012-05-03 22:18:31 +0000

    made language setup to match CI standards.

---
+++ trunk/application/language/english/english.lang.js
@@ -0,0 +1,58 @@
+/**
+Filename: English.lang.js
+Last Modified: 02/20/2012
+
+Term of Use:
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+*/
+
+var lang = {
+	fbWarning: "Firebug is known to interact with this website, please disable it to prevent any problems while visiting this site.",
+	nospellerrors: "There are no incorrectly spelt words.",
+    dlgHelp: "Help",
+    jsError: "Sorry but there was an error",
+    enableRTF: "Enable Rich Text Editor",
+    disableRTF: "Disable Rich Text Editor",
+    dlgAdvSearch: "Advanced Search",
+    dlgCancel: "Cancel",
+    dlgSmilesList: "Smiles List",
+    dlgUploadMgr: "Attachment Manager",
+    attachDeleted: "File was deleted successfully.",
+    attachSuccess: "File Uploaded.",
+    attachFailed: "Upload Failed.",
+    attachPending: "Pending...",
+    attachLoading: "Uploading File...",
+    attachLimit: "You have reached your upload limit.",
+	confDel: "Confirm Deletion",
+	genSuccess: "Success!",
+	cfmDel: "Are you sure you wish to delete this?",
+	Yes: "Yes",
+	No: "No",
+	reloginTitle: "Re-login Notice",
+	logoffNotice: "You need to re-login to finalize the password change.",
+	Ok: 'Ok',
+	archiveConf: 'Archive message, are you sure?',
+	//VALIDATION
+	required: 'The %s field is required.',
+	matches: 'The %s field does not match the %s field.',
+	valid_email: 'The %s field must contain a valid email address.',
+	valid_emails: 'The %s field must contain all valid email addresses.',
+	min_length: 'The %s field must be at least %s characters in length.',
+	max_length: 'The %s field must not exceed %s characters in length.',
+	exact_length: 'The %s field must be exactly %s characters in length.',
+	greater_than: 'The %s field must contain a number greater than %s.',
+	less_than: 'The %s field must contain a number less than %s.',
+	alpha: 'The %s field must only contain alphabetical characters.',
+	alpha_numeric: 'The %s field must only contain alpha-numeric characters.',
+	alpha_dash: 'The %s field must only contain alpha-numeric characters, underscores, and dashes.',
+	numeric: 'The %s field must contain only numbers.',
+	integer: 'The %s field must contain an integer.',
+	decimal: 'The %s field must contain a decimal number.',
+	is_natural: 'The %s field must contain only positive numbers.',
+	is_natural_no_zero: 'The %s field must contain a number greater than zero.',
+	valid_ip: 'The %s field must contain a valid IP.',
+	valid_base64: 'The %s field must contain a base64 string.'
+};
